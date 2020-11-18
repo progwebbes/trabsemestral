06fe218bd2010c6281ddb333d38b160f82f49d11
Raggi Izar <61994369+Raggiiz@users.noreply.github.com>
2020-11-18 18:52:10 -0300

    tudo arrumado

--- js/script.js
+++ js/script.js
@@ -4,4 +4,19 @@ $(document).ready(function() {
         $('.slogan').css('display','none')
     });
 
+    $('#confirmaCompra').click(function(){
+        $('.pagamento-container').css('display','none');
+        $('.container-sucesso').css('display','block');
+    });
+
+    $('#cadastrarDisplay').click(function(){
+        $('#loginForm').css('display','none');
+        $('#cadastroForm').css('display','block');
+    });
+
+    $('#loginDisplay').click(function(){
+        $('#loginForm').css('display','block');
+        $('#cadastroForm').css('display','none');
+    });
+
 });
